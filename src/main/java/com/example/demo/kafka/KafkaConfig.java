6be10756a0d00c4ad9ca8ab20e31528051c2d39d
Michal Paukszto <michal.paukszto@gmail.com>
2021-02-15 10:29:34 +0100

    reactive kafka - string type of messages

--- src/main/java/com/example/demo/kafka/KafkaConfig.java
+++ src/main/java/com/example/demo/kafka/KafkaConfig.java
@@ -4,18 +4,21 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 import reactor.kafka.receiver.KafkaReceiver;
 import reactor.kafka.receiver.ReceiverOptions;
 import reactor.kafka.receiver.internals.ConsumerFactory;
 import reactor.kafka.receiver.internals.DefaultKafkaReceiver;
-import reactor.kafka.sender.KafkaSender;
 import reactor.kafka.sender.SenderOptions;
 
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.Collections;
 import java.util.Map;
 
@@ -31,7 +34,7 @@ public class KafkaConfig {
     }
 
     @Bean
-    KafkaReceiver kafkaReceiver(KafkaProperties kafkaProperties) {
+    KafkaReceiver<String, String> kafkaReceiver(KafkaProperties kafkaProperties) {
         Map<String, Object> props = kafkaProperties.buildProducerProperties();
         props.put(ConsumerConfig.GROUP_ID_CONFIG, SPRING_GROUP_ID);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
@@ -42,12 +45,13 @@ public class KafkaConfig {
     }
 
     @Bean
-    KafkaSender<String, String> kafkaSender(KafkaProperties kafkaProperties) {
-        Map<String, Object> props = kafkaProperties.buildProducerProperties();
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    ReactiveKafkaProducerTemplate<String, Object> reactiveKafkaProducerTemplate(ProducerFactory<String, Object> producerFactory) {
+        Map<String, Object> properties = producerFactory.getConfigurationProperties();
+        SenderOptions<String, Object> senderOptions = SenderOptions.<String, Object>create(properties)
+                .withValueSerializer(new JsonSerializer<>())
+                .closeTimeout(Duration.of(5, ChronoUnit.MINUTES))
+                .producerProperty(ProducerConfig.RETRIES_CONFIG, 10);
 
-        SenderOptions<String, String> senderOptions = SenderOptions.create(props);
-        return KafkaSender.create(senderOptions);
+        return new ReactiveKafkaProducerTemplate<>(senderOptions);
     }
 }
--- src/main/java/com/example/demo/kafka/KafkaPublisher.java
+++ src/main/java/com/example/demo/kafka/KafkaPublisher.java
@@ -1,14 +1,11 @@
 package com.example.demo.kafka;
 
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
-import reactor.kafka.sender.KafkaSender;
-import reactor.kafka.sender.SenderRecord;
 
 import java.time.Duration;
 
@@ -19,14 +16,12 @@ import static com.example.demo.kafka.KafkaConfig.SPRING_TOPIC;
 @RequiredArgsConstructor
 public class KafkaPublisher implements CommandLineRunner {
 
-    private final KafkaSender<String, String> sender;
+    private final ReactiveKafkaProducerTemplate<String, Object> producerTemplate;
 
     @Override
-    @SneakyThrows
     public void run(String... args) {
-        var flux = Flux.interval(Duration.ofSeconds(3))
-                .map(i -> SenderRecord.create(new ProducerRecord<>(SPRING_TOPIC, "Key_" + i, "Message_" + i), i));
-
-        sender.send(flux).subscribe();
+        Flux.interval(Duration.ofSeconds(3))
+                .flatMap(i -> producerTemplate.send(SPRING_TOPIC, "message-" + i))
+                .subscribe();
     }
 }
